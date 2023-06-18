3e52e0d899be5aecbff17ccce460b82f295aa298
Oneil Benjamin <oneilbenjamin@Oneils-MacBook-Air-2.local>
2023-06-17 22:03:24 -0500

    master

---
+++ _next/static/zWJf_LIVBfpAkpONSqs2K/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/46-26e50c03eec207d1.js","static/chunks/pages/index-a3739a53da1e5df8.js"],"/_error":["static/chunks/pages/_error-effe22be6ff34abe.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
